df1c70a8e01f22f09f6d0c1e73f4ce22ea51d133
Yurii-Ilnytskyi22 <mister.ilnitskiy@gmail.com>
2026-05-12 19:22:53 +0200

    Terakot2.0-0.0

---
+++ js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerBtn = document.getElementById('burgerBtn');
+  const closeBtn = document.getElementById('closeBtn');
+  const mobileMenu = document.getElementById('mobileMenu');
+
+  // Відкриття
+  burgerBtn.addEventListener('click', () => {
+    mobileMenu.classList.add('is-open');
+    document.body.style.overflow = 'hidden'; // Щоб сторінка не скролилась під меню
+  });
+
+  // Закриття
+  closeBtn.addEventListener('click', () => {
+    mobileMenu.classList.remove('is-open');
+    document.body.style.overflow = ''; 
+  });
+
+  // Закриття при кліку на посилання
+  const menuLinks = document.querySelectorAll('.mobile-nav a');
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      mobileMenu.classList.remove('is-open');
+      document.body.style.overflow = '';
+    });
+  });
+});
